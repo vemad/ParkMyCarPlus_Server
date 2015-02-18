b3fbaf718c93436f226db832e0789de7c054a0fc
Stephane_KI <ki.stephane.dambi@gmail.com>
2015-02-18 11:59:34 +0100

    First commit; project created

---
+++ src/main/java/hello/Place.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Place {
+
+    private final long id;
+    private final String content;
+
+    public Place(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
