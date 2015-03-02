cd1b2b24ec75215c56a28173995b46a96ac22851
Stephane_KI <ki.stephane.dambi@gmail.com>
2015-03-02 10:04:21 +0100

    Error handled by copying and pasting

--- src/main/java/com/PlaceController.java
+++ src/main/java/com/PlaceController.java
@@ -3,80 +3,91 @@ package com;
 import com.pmc.config.DAOManager;
 import com.pmc.dao.PlaceDAO;
 import com.pmc.model.Place;
-import com.util.Message4Client;
-import com.util.Position;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.util.*;
 
 import java.util.List;
 
-/**
- *
- * @author gaetandeshayes
- */
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 public class PlaceController {
 
     private static final int defaultRadius = 100;
     private static final int maxRadius = 1000;
+    private static final int maxRadiusOfPlace = 3;
 
-    /**
-     * Route mapping to retrieve a position knowing its id.
-     * @param id The id of the place to be found
-     * @return The place founded or null otherwise
-     */
     @RequestMapping("/place")
     public Place place(@RequestParam(value="id") int id) {
 
         PlaceDAO placeDAO = (PlaceDAO)DAOManager.getDAOManager().getDao(DAOManager.TypeDAO.PLACE);
-        return new Place(); //TODO Do not keep this
-        //return placeDAO.findById(id);
+
+        return placeDAO.findById(id);
     }
 
-    /**
-     *
-     * @param body
-     * @return
-     */
     @RequestMapping(value ="/place/released", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<Message4Client> placeReleased(@RequestBody Position body ){
+    public @ResponseBody ResponseEntity<Message4Client> placeReleased(@RequestBody Position position ){
 
-        //TODO Implement this method
-        return new ResponseEntity(new Message4Client("OK"), new HttpHeaders(), HttpStatus.OK);
+        PlaceDAO placeDAO = (PlaceDAO)DAOManager.getDAOManager().getDao(DAOManager.TypeDAO.PLACE);
+        List<Place> listNearestPlace = placeDAO.findNearestPlaces(position.getLatitude(), position.getLongitude(), maxRadiusOfPlace);
+
+        //If no nearest place found do nothing
+        if(listNearestPlace.isEmpty()){
+            return new ResponseEntity(new Message4Client("Place not found"), new HttpHeaders(), HttpStatus.NOT_FOUND);
+        }
+        else{ //else release the first: the nearest place
+            Place place = listNearestPlace.get(0);
+            if(place.isTaken()){
+                place.releasePlace();
+                placeDAO.savePlace(place);
+                return new ResponseEntity(new Message4Client("Place released"), new HttpHeaders(), HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity(new Message4Client("Place already released"), new HttpHeaders(), HttpStatus.CONFLICT);
+            }
+        }
     }
 
-    /**
-     *
-     * @param body
-     * @return
-     */
     @RequestMapping(value ="/place/taken", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<Message4Client> placeTaken(@RequestBody Position body ){
+    public @ResponseBody ResponseEntity<Place> placeTaken(@RequestBody Position position ){
 
-        //TODO Implement this method
-        return new ResponseEntity(new Message4Client("OK"), new HttpHeaders(), HttpStatus.OK);
+        PlaceDAO placeDAO = (PlaceDAO)DAOManager.getDAOManager().getDao(DAOManager.TypeDAO.PLACE);
+        List<Place> listNearestPlace = placeDAO.findNearestPlaces(position.getLatitude(), position.getLongitude(), maxRadiusOfPlace);
+
+        //If no nearest place found,create one
+        if(listNearestPlace.isEmpty()){
+            Place newPlace = new Place();
+            newPlace.setLatitude(position.getLatitude()).setLongitude(position.getLongitude()).takePlace();
+            placeDAO.savePlace(newPlace);
+
+            return new ResponseEntity(newPlace, new HttpHeaders(), HttpStatus.OK);
+        }
+        else{ //else take the first: the nearest place
+            //TODO Change the algo to manager better the taking of a place use
+            Place place = listNearestPlace.get(0);
+            if(place.isTaken()){
+                return new ResponseEntity(null, new HttpHeaders(), HttpStatus.CONFLICT);
+            }
+            else{
+                place.takePlace();
+                placeDAO.savePlace(place);
+                return new ResponseEntity(place, new HttpHeaders(), HttpStatus.OK);
+            }
+        }
     }
 
 
-    /**
-     * Route mapping to collect all the places around the position of teh driver. These places have to
-     * be founded in a certain circle represented by the radius.
-     * @param longitude longitude of the driver position
-     * @param latitude latitude of the driver position
-     * @param radius radius of the circle zone in which places have to be found
-     * @return List of all the places in a circle knowing the circle center and the radius.
-     */
     @RequestMapping("/places")
-    public List<Place> listPlacesByPosition(@RequestParam(value="longitude") double longitude,
-                                            @RequestParam(value="latitude") double latitude,
-                                            @RequestParam(value="radius", defaultValue="0")
-                                            int radius){
+    public List<Place> listPlacesByPosition(@RequestParam(value="latitude") double latitude,
+                                            @RequestParam(value="longitude") double longitude,
+                                            @RequestParam(value="radius", defaultValue="0") int radius){
 
         if(radius<1 || radius>maxRadius) radius = defaultRadius;
 
         PlaceDAO placeDAO = (PlaceDAO)DAOManager.getDAOManager().getDao(DAOManager.TypeDAO.PLACE);
-        return placeDAO.findPlacesByPosition(longitude, latitude, radius);
+        return placeDAO.findPlacesByPosition(latitude, longitude, radius);
     }
 }
